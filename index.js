abd9967602c1de72dca1a26443c10582b78c9293
Lucas Costa <lucas.costa@waproject.com.br>
2022-08-18 02:31:39 -0300

    adding cors lib

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const controller = require("./src/controller");
 
 const app = express();
 const port = 4000;
+
+app.use(cors());
 
 app.use("/", controller);
 
